1399bf52c738d2c1447b186a68182a3d29dd36db
Charles Thomas Bowden <charles.bowden@gmail.com>
2018-01-11 13:42:37 -0500

    added path

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 // =============================================================
 var express = require("express");
 var bodyParser = require("body-parser");
-var path = require("path");
 
 
 // Sets up the Express App with port detection for deployment or 3000 locally
