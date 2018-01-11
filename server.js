5f380bb24dbe01f33511039f05776bf6af95f693
Charles Thomas Bowden <charles.bowden@gmail.com>
2018-01-11 15:51:16 -0500

    Updates

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Routes
